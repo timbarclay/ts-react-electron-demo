f5c76fef6d5780979198eddba94a7dbd513e678c
Tim Barclay <tim.barclay@aeriandi.com>
2017-03-09 17:58:05 +0000

    Change to using type definitions from npm

--- src/main/main.ts
+++ src/main/main.ts
@@ -10,7 +10,7 @@ const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let mainWindow: Electron.BrowserWindow;
 
 function createWindow () {
   // Create the browser window.
--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -2,7 +2,7 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-var body = document.querySelector("body");
-var div = document.createElement("div");
-div.classList.add("content");
+const body = document.querySelector("body");
+const div = document.createElement("div");
+div.classList.add("app-content");
 body.appendChild(div);
