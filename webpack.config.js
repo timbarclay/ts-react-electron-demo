294801d9215d8967665f10ea0a5c8e2d86a7c7ea
Tim Barclay <tim.barclay@aeriandi.com>
2017-03-09 17:31:32 +0000

    Upgrade all the dependencies and migrate to webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-var webpack = require('webpack');
-const production = process.env.NODE_ENV === "production";
+const webpack = require('webpack');
+const environment = process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase().trim() : "development";
+const production = environment === "production";
 const ExtractPlugin = require("extract-text-webpack-plugin");
 const CleanPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -10,7 +11,7 @@ var plugins = [
   new webpack.DefinePlugin({
     __DEVELOPMENT__: !production,
     "process.env": {
-      "NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+      "NODE_ENV": JSON.stringify(environment)
     }
   }),
   new webpack.ProvidePlugin({
@@ -21,12 +22,10 @@ var plugins = [
   })
 ];
 
-console.log("Building for " + process.env.NODE_ENV);
+console.log(`Building for ${environment}`);
 
 if(production){
   plugins = plugins.concat([
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.MinChunkSizePlugin({
       minChunkSize: 51200
     }),
@@ -40,7 +39,7 @@ if(production){
 }
 
 module.exports = {
-  target: 'electron-renderer',
+  target: "electron-renderer",
 
   node: {
     __filename: false,
@@ -56,22 +55,33 @@ module.exports = {
     filename: "[name]/bundle.js",
   },
 
-  debug: !production,
   devtool: production ? false : "source-map",
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
   },
 
   module: {
-    loaders: [
-      { test: /\.tsx?$/, loader: "ts-loader" },
+    rules: [
+      { 
+        test: /\.tsx?$/, 
+        loader: "ts-loader" 
+      },
       { 
         test: /\.scss/,
-        loader: ExtractPlugin.extract("style", "css!sass") 
+        use: ExtractPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader', 
+            'sass-loader'
+          ]
+        }) 
       },
-      { test: /\.html/, loader: "html" },
+      { 
+        test: /\.html/,
+        loader: "html" 
+      },
       { 
         test: /\.(png|gif|jpe?g|svg)$/i,
         loader: "url?limit=10000"
@@ -79,10 +89,12 @@ module.exports = {
       {
         test: /\.(eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/,
         loader: 'url-loader?limit=10000'
+      },
+      { 
+        test: /\.js$/, 
+        enforce: "pre",
+        loader: "source-map-loader" 
       }
-    ],
-    preLoaders: [
-      { test: /\.js$/, loader: "source-map-loader" }
     ]
   },
 
